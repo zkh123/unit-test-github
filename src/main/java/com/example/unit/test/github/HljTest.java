75bce3f48f07acacf5da66f214e74ef2dcf8d4e2
huanglijun@ppdai.com <huanglijun@ppdai.com>
2020-11-25 17:44:59 +0800

    add something

--- src/main/java/com/example/unit/test/github/HljTest.java
+++ src/main/java/com/example/unit/test/github/HljTest.java
@@ -3,9 +3,21 @@ package com.example.unit.test.github;
 public class HljTest {
     public static void main(String[] args) {
         test01();
+        test02();
     }
 
     private static void test01(){
+        System.out.println("HljStep01 我搞点事情！");
+        String startVersion = "3.2.0";
+        String appVersion = "3.1.9";
+        if(appVersion.compareToIgnoreCase(startVersion)>=0){
+            System.out.println(false);
+        }else {
+            System.out.println(true);
+        }
+    }
+
+    private static void test02(){
         System.out.println("HljStep01 我搞点事情！");
         String startVersion = "3.2.0";
         String appVersion = "3.1.9";
