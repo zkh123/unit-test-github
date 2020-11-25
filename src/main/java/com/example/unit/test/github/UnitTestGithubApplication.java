74a0150dfe3d6b12214964c573c1ae790708d2c3
huanglijun@ppdai.com <huanglijun@ppdai.com>
2020-11-25 16:35:18 +0800

    update

--- src/main/java/com/example/unit/test/github/UnitTestGithubApplication.java
+++ src/main/java/com/example/unit/test/github/UnitTestGithubApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UnitTestGithubApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(UnitTestGithubApplication.class, args);
     }
-
 }
