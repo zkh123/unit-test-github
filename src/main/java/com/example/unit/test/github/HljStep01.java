195d677d907125d58de2d5a0b456748d7dbd417e
huanglijun@ppdai.com <huanglijun@ppdai.com>
2020-11-25 16:31:14 +0800

    update

---
+++ src/main/java/com/example/unit/test/github/HljStep01.java
@@ -0,0 +1,13 @@
+package com.example.unit.test.github;
+
+public class HljStep01 {
+
+    public static void main(String[] args) {
+        test01();
+    }
+
+    private static void test01(){
+        System.out.println("hello world 1125_step01");
+    }
+
+}
--- src/main/java/com/example/unit/test/github/HljTest.java
+++ src/main/java/com/example/unit/test/github/HljTest.java
@@ -3,11 +3,22 @@ package com.example.unit.test.github;
 public class HljTest {
     public static void main(String[] args) {
         test01();
+        test02();
     }
 
     private static void test01(){
         String startVersion = "3.2.0";
+        String appVersion = "3.1.9";
+        if(appVersion.compareToIgnoreCase(startVersion)<0){
+            System.out.println(true);
+        }else {
+            String param = "Hello World!";
+            System.out.println(false);
+        }
+    }
 
+    private static void test02(){
+        String startVersion = "3.2.0";
         String appVersion = "3.1.9";
         if(appVersion.compareToIgnoreCase(startVersion)>=0){
             System.out.println(false);
