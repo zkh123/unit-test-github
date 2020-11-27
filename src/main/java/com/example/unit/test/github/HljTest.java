44078731f11c41aab67d926d1f8f22c65d99f2de
huanglijun@ppdai.com <huanglijun@ppdai.com>
2020-11-27 21:06:39 +0800

    test

--- src/main/java/com/example/unit/test/github/HljTest.java
+++ src/main/java/com/example/unit/test/github/HljTest.java
@@ -1,8 +1,39 @@
 package com.example.unit.test.github;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class HljTest {
+
+    public final static Integer Withdraw_Success =1;
+
     public static void main(String[] args) {
-        test01();
+//        test01();
+        test2();
+    }
+
+    private static void test2(){
+        List<ListAndWithdrawInfoDto> listAndWithdrawInfoDtos = new ArrayList<>();
+        ListAndWithdrawInfoDto listAndWithdrawInfoDto1 = new ListAndWithdrawInfoDto();
+        listAndWithdrawInfoDto1.setWithdrawStatus(0);listAndWithdrawInfoDto1.setBorrowerId(111134343l);listAndWithdrawInfoDto1.setListingId(2143244l);
+        listAndWithdrawInfoDtos.add(listAndWithdrawInfoDto1);
+        ListAndWithdrawInfoDto listAndWithdrawInfoDto2 = new ListAndWithdrawInfoDto();
+        listAndWithdrawInfoDto2.setWithdrawStatus(1);listAndWithdrawInfoDto2.setBorrowerId(111134343l);listAndWithdrawInfoDto2.setListingId(2143244l);
+        listAndWithdrawInfoDtos.add(listAndWithdrawInfoDto2);
+        ListAndWithdrawInfoDto listAndWithdrawInfoDto3 = new ListAndWithdrawInfoDto();
+        listAndWithdrawInfoDto3.setWithdrawStatus(1);listAndWithdrawInfoDto3.setBorrowerId(111134343l);listAndWithdrawInfoDto3.setListingId(2143244l);
+        listAndWithdrawInfoDtos.add(listAndWithdrawInfoDto3);
+
+        List<ListAndWithdrawInfoDto> collect= listAndWithdrawInfoDtos.stream()
+                .filter(ele -> ele.getWithdrawStatus() != null)
+                .filter(ele -> ele.getWithdrawStatus() == 1)
+                .collect(Collectors.toList());
+        if (collect.size() == 0) { //新客.没有提现成功过
+            System.out.println("新客");
+        }else{
+            System.out.println("老客");
+        }
     }
 
     private static void test01(){
---
+++ src/main/java/com/example/unit/test/github/ListAndWithdrawInfoDto.java
@@ -0,0 +1,31 @@
+package com.example.unit.test.github;
+
+public class ListAndWithdrawInfoDto {
+    private Long borrowerId;
+    private Long listingId;
+    private Integer withdrawStatus;
+
+    public Long getBorrowerId() {
+        return borrowerId;
+    }
+
+    public void setBorrowerId(Long borrowerId) {
+        this.borrowerId = borrowerId;
+    }
+
+    public Long getListingId() {
+        return listingId;
+    }
+
+    public void setListingId(Long listingId) {
+        this.listingId = listingId;
+    }
+
+    public Integer getWithdrawStatus() {
+        return withdrawStatus;
+    }
+
+    public void setWithdrawStatus(Integer withdrawStatus) {
+        this.withdrawStatus = withdrawStatus;
+    }
+}
